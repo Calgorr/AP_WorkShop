974adbf7340453e60c013991688b98ff1a65851a
Calgorr <91310297+Calgorr@users.noreply.github.com>
2022-03-13 11:19:58 +0330

    added methods

--- AP_HW4/Person.java
+++ AP_HW4/Person.java
@@ -8,4 +8,16 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String toString() {
+        return firstName + lastName;
+    }
 }
