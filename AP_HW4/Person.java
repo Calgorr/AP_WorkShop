4d3dca584245114087741af615148f3400302fd5
Calgorr <91310297+Calgorr@users.noreply.github.com>
2022-03-13 11:23:15 +0330

    Done with person

--- AP_HW4/Person.java
+++ AP_HW4/Person.java
@@ -13,8 +13,16 @@ public class Person {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String toString() {
